d00f80952e8036cff65fd7f764d7f700c0e17473
Simon Gerber <simugerber@student.ethz.ch>
2012-01-03 11:17:14 +0100

    Implemented SkyBlock Challenge manager for Skyblock 2.1.

--- skyblock.py
+++ skyblock.py
@@ -1,8 +1,45 @@
 #!/usr/bin/python
 from flask import Flask, render_template, request, redirect, url_for
+import pickle
+
+class Challenge(object):
+    def __init__(self, id, desc, checked):
+        self.id = id
+        self.desc = desc
+        self.checked = checked
+        pass
+
+    def __str__(self):
+        return str(self.id+1) + ") " + self.desc
+
+    def __repr__(self):
+        return self.__str__() + " [" + str(self.checked) + "]"
+
 
 skyblock = Flask(__name__)
-checked_boxes = []
+
+challenges = [None] * 50
+
+# read challenge descriptions
+def init_challenges():
+    with open("challenges.txt") as cfile:
+        chtxts = map(str.strip, cfile.readlines())
+    for i in xrange(50):
+        challenges[i] = Challenge(i, chtxts[i], False)
+
+def save_challenges():
+    checked = [ c.id for c in challenges if c.checked ]
+    with open("store.txt", "w") as f:
+        pickle.dump(checked, f)
+
+def load_challenges():
+    try:
+        with open("store.txt") as f:
+            checked = pickle.load(f)
+    except:
+        checked = []
+    for i in checked:
+        challenges[i].checked = True
 
 @skyblock.route("/favicon.ico")
 def favicon():
@@ -10,15 +47,26 @@ def favicon():
 
 @skyblock.route("/")
 def index():
-    return render_template('index.html', checked=checked_boxes)
+    return render_template('index.jhtml', challenges=challenges, version="2.1")
 
 @skyblock.route("/store", methods=['POST'])
 def store():
     if request.method == "POST":
-        checked_boxes = request.data.split(',')[:-1]
-        return "Saving succeeded"
+        try:
+            checked_boxes = map(int, request.data.split(',')[:-1])
+            for i in checked_boxes:
+                challenges[i].checked = True
+
+            save_challenges()
+
+            return "Saving succeeded"
+        except Exception, e:
+            print e
+            return str(e)
     else:
         return "Only accepts POST"
 
 if __name__ == "__main__":
+    init_challenges()
+    load_challenges()
     skyblock.run(debug=True)
---
+++ static/save.js
@@ -0,0 +1,19 @@
+
+function store() {
+	data = "";
+	for (i = 0; i < 50; i++) {
+		if($("#cb"+i).is(":checked")) {
+			data += i+",";
+		}
+	}
+	r = new XMLHttpRequest();
+	r.onreadystatechange=function(){
+		if(r.readyState==4 && r.status==200) {
+			$("#responsediv").html(r.responseText);	
+			$("#responsediv").fadeIn().delay(1500).fadeOut()
+		}
+	}
+	r.open("POST", "/store", true);
+	r.send(data)
+}
+
